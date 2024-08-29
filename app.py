6f444906e84449d94e663d05121a9c1447454eae
Marwen <44669234+marwen996@users.noreply.github.com>
2024-08-29 12:10:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
-    st.subheader("Total Sales:")
-    st.subheader(f"US $ {total_sales:,}")
+    st.subheader("Machine:")
+    st.subheader("IPS01")
 with middle_column:
     st.subheader("Average Rating:")
     st.subheader(f"{average_rating} {star_rating}")
